ce5436ec1cc011113dbc30700260df6e92d73d20
RulaMikkawi <rula.mikkawi@gmail.com>
2017-10-01 17:16:42 +0200

    Part1 Final

--- Cal1.fs
+++ Cal1.fs
@@ -2,13 +2,15 @@ open System
 
 let Add (numbers : String) :int = 
    match numbers with 
-    | " " -> 0
+    | "" -> 0
     | _ when numbers.Contains "," -> 
        let n = numbers.Split [|','|]
        (Int32.Parse n.[0]) + (Int32.Parse n.[1])
-    | _ -> Int32.Parse numbers
+    | _ when numbers.Length=1 -> Int32.Parse numbers
 
-    
+
+
+Add "1,1";;
 
 
 
