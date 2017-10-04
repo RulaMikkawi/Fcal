a9ff4254c5917b35e509ef9ded68983a41ea3977
RulaMikkawi <rula.mikkawi@gmail.com>
2017-10-04 11:50:43 +0200

    5th part

--- Cal1.fs
+++ Cal1.fs
@@ -1,18 +1,25 @@
+
 open System
 
-let Add (numbers : String) : int =
- 
- let sumArr = Array.fold (fun acc elem -> acc + (int elem))  0
- 
- match numbers with
-   | "" -> 0  
-   | _ when numbers.StartsWith "//" ->
-       let x= (numbers.IndexOf "//") + 1
-       let arr1 = numbers.Substring  ((numbers.IndexOf "\n") + 1 )
-       arr1.Split [| numbers.[3] |] |> sumArr
-   | _ ->
-      numbers.Split [|',' ; '\n'|] |> sumArr
-      
+type  CountNeg (negNums) =
+   inherit Exception(sprintf "negatives anre not allowed %A" negNums)  
+
+type SumNum(numbers : string) = 
+   let rec Add2 delimiters numbers = 
+       match numbers with
+       | "" -> 0
+       | _ when numbers.StartsWith "//" -> 
+            numbers.Substring  ((numbers.IndexOf "\n") + 1 ) |> Add2 [|numbers.[2]|]
+
+       | _ -> 
+          let negNums, posNums = 
+            [for i in numbers.Split delimiters -> Int32.Parse i] |> List.partition (fun i -> i <0)
+          if negNums.Length > 0 then raise (CountNeg negNums)
+
+          posNums |> List.reduce( fun acc elem -> acc + (int elem)) 
+
+   member x.Add numbers =  numbers |> Add2 [| ',' ; '\n'|]  
 
 
-Add "//@\n3@3"
+
+
