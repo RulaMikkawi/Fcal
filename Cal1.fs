c3c0c54491a952bf1faa1f784f969eb7dec9c2e4
RulaMikkawi <rula.mikkawi@gmail.com>
2017-10-07 14:25:14 +0200

    part 6

--- Cal1.fs
+++ Cal1.fs
@@ -5,9 +5,11 @@ exception IsNeg of int[]
 
 let SumNum (numbers :string) =
      let  x=numbers.Substring  ((numbers.IndexOf "\n") + 1 ) 
+     
      x.Split [|numbers.[2]|]  
  
    |> Array.map Int32.Parse 
+   |> Array.filter(fun i -> i <= 1000 )
    |> Array.sum
 
   
@@ -15,9 +17,8 @@ let SumNeg numbers  =
    match numbers |> Array.filter (fun n -> n <0) with
      | negNums when negNums.Length> 0 -> raise (IsNeg negNums) 
      | _ -> numbers
- 
-
-
+  
+   
 let Add (numbers : string) =
    
    match numbers with 
@@ -27,7 +28,8 @@ let Add (numbers : string) =
 
             
 
-Add "//;\n2;-3;5"
+Add "//,\n4,-1,10001"
+
 
 
 
